1c2cb90883ae00903bb93c2a6dde09573e2f9b47
akhdanrgya <akhdan.anargya@gmail.com>
2024-05-03 20:48:28 +0700

    update GUI adding admin btn

--- gui.py
+++ gui.py
@@ -5,30 +5,46 @@ class GUI:
     
     def __init__(self):
         
-        def click():
-            self.screen = tk.Tk()
-            self.screen.title("Anjay di click")
+        def adminPin():
+            self.root_admin = tk.Toplevel()
+            self.root_admin.title("Admin panel")
 
-            self.label = tk.Label(self.screen, text=self.i.get())
-            self.label.pack(padx=20, pady=20)
+            self.label_admin = Label(self.root_admin, text="Enter PIN:", font="Arial")
+            self.label_admin.pack(padx=50, pady=10)
+
+            self.pin_admin = Entry(self.root_admin, show="*")
+            self.pin_admin.pack(padx=50, pady=5)
+
+            self.btn_admin = Button(self.root_admin, text="Enter", command=checkPin)
+            self.btn_admin.pack(padx=50, pady=10)
+        
+        def checkPin():
+            pin = self.pin_admin.get()
+
+            if pin == "123":
+                # self.label_admin.config(text="ADMIN PANEL RENTAL KENDARAAN", font="Arial")
+                self.root_admin.destroy()
+                adminPanel()
+            else:
+                self.label_admin.config(text="Incorrect PIN. Try again.", font="Arial", fg="red")
+                self.pin_admin.delete(0, 'end')
+        
+        def adminPanel():
+            self.rootAdminPanel = tk.Tk()
+            self.rootAdminPanel.title("ADMIN PANEL RENTAL KENDARAAN")
             
-        self.screen = tk.Tk()
-        self.screen.title("Rental Kendaraan")
-        
-        self.i = Entry(self.screen, width=50)
-        self.i.pack()
+            self.adminLabel = Label(self.rootAdminPanel, text="ADMIN PANEL RENTAL KENDARAAN", font="Arial")
+            self.adminLabel.pack(padx=50, pady=20)
 
-        self.label = tk.Label(self.screen, text="RENTAL APP", font=('Arial', 18))
-        self.label.pack(padx=20, pady=20)
+        self.root = tk.Tk()
+        self.root.title("Rental Kendaraan")
         
-        self.btn1 = Button(self.screen, text="click aku dong kak", command=click, fg="black", bg="gray")
-        self.btn1.pack()
+        self.label = Label(self.root, text="RENTAL KENDARAAN", font="Arial")
+        self.label.pack(padx=50, pady=20)
         
-        for x in range(5):
-            self.btn2 = Button(self.screen, text=f"loop ke {x + 1}")
-            self.btn2.pack()
-
-        self.screen.mainloop()
-
+        self.adminBTN = Button(self.root, text="Admin", command=adminPin)
+        self.adminBTN.pack(padx=10, pady=10, side="left")
+        
+        self.root.mainloop()
 
 GUI()
